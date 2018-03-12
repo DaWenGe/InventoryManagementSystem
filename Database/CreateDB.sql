ee6858c9a7fb9377efef28f317bb6bb821ab8d2f
Angad Thandi <angadthandi8@gmail.com>
2018-03-11 21:19:47 -0600

    Add User & UserTypes Tables

--- Database/CreateDB.sql
+++ Database/CreateDB.sql
@@ -1,3 +1,20 @@
 CREATE DATABASE IF NOT EXISTS InventorySystemDB;
 
-USE InventorySystemDB
+USE InventorySystemDB;
+
+CREATE TABLE IF NOT EXISTS UserTypes (
+	UserTypeID int NOT NULL,
+    UserTypeName varchar(100) NOT NULL,
+    PRIMARY KEY (UserTypeID)
+);
+
+CREATE TABLE IF NOT EXISTS Users (
+    UserID int NOT NULL,
+    UserName varchar(100) NOT NULL,
+    Password varchar(255) NOT NULL,
+    FirstName varchar(100) NULL,
+    Email varchar(100) NULL,
+    UserTypeID int NOT NULL,
+    PRIMARY KEY (UserID),
+    FOREIGN KEY (UserTypeID) REFERENCES UserTypes (UserTypeID)
+);
