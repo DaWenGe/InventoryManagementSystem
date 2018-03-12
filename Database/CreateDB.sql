d1b53de0871a15132fc2d1c7bf0742f30a830463
Angad Thandi <angadthandi8@gmail.com>
2018-03-11 22:11:09 -0600

    Create more tables

--- Database/CreateDB.sql
+++ Database/CreateDB.sql
@@ -38,6 +38,28 @@ CREATE TABLE IF NOT EXISTS ProductColors (
 	ProductColorID int NOT NULL,
     ProductID int NOT NULL,
     ColorID int NOT NULL,
+    PRIMARY KEY (ProductColorID),
     FOREIGN KEY (ProductID) REFERENCES Products (ProductID),
     FOREIGN KEY (ColorID) REFERENCES Colors (ColorID)
+);
+
+CREATE TABLE IF NOT EXISTS DeliveryStatus (
+	DeliveryStatusID int NOT NULL,
+    Status VARCHAR(50) NOT NULL,
+    PRIMARY KEY (DeliveryStatusID)
+);
+
+CREATE TABLE IF NOT EXISTS PaymentType (
+	PaymentTypeID int NOT NULL,
+    PaymentMethod VARCHAR(100) NOT NULL,
+    PRIMARY KEY (PaymentTypeID)
+);
+
+CREATE TABLE IF NOT EXISTS Inventory (
+	InventoryID int NOT NULL,
+    ProductColorID int NOT NULL,
+    Quantity int NOT NULL,
+    Price DECIMAL(10,2) NOT NULL,
+    PRIMARY KEY (InventoryID),
+    FOREIGN KEY (ProductColorID) REFERENCES ProductColors (ProductColorID)
 );
