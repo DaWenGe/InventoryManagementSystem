d5ad4677752cf449cba3c44cbb18e3a7d51a3451
Angad Thandi <angadthandi8@gmail.com>
2018-04-01 19:47:08 -0600

    Webpack setup files

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var webpack = require("webpack");
+var path = require("path");
+
+var DIST_DIR = path.resolve(__dirname, "dist");
+var SRC_DIR = path.resolve(__dirname, "source");
+
+var config = {
+	entry: SRC_DIR + "/js/app.js",
+	output: {
+		path: DIST_DIR + "/js",
+		filename: "bundle.js",
+		publicPath: "/js/"
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js?/,
+				include: SRC_DIR,
+				loader: "babel-loader",
+				query: {
+					presets: ["react", "es2015", "stage-2"]
+				}
+			}
+		]
+	}
+};
+
+module.exports = config;
